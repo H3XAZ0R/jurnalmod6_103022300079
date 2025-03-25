199953c0d50c2d99f92b5f1e07e5babb2d2e5c5b
syahdan <syahdanmk1234@gmail.com>
2025-03-25 07:22:19 +0700

    menambahkan SayaTubeVideo

---
+++ jurnalmod6/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(string title)
+        {
+            if (string.IsNullOrEmpty(title) || title.Length > 200)
+                throw new ArgumentException("Judul video tidak boleh kosong dan maksimal 200 karakter.");
+
+            Random random = new Random();
+            this.id = random.Next(10000, 99999);
+            this.title = title;
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+            if (count < 0 || count > 25000000)
+                throw new ArgumentException("Play count harus positif dan maksimal 25.000.000.");
+
+            try
+            {
+                checked { this.playCount += count; }
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Terjadi overflow saat menambahkan play count.");
+            }
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine($"ID Video: {id}");
+            Console.WriteLine($"Judul: {title}");
+            Console.WriteLine($"Jumlah Play: {playCount}");
+        }
+
+        public int GetPlayCount()
+        {
+            return playCount;
+        }
+
+        public string GetTitle()
+        {
+            return title;
+        }
+    }
+}
+    
+   
---
+++ jurnalmod6/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("jurnalmod6")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("jurnalmod6")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f55946df-095c-4261-a53b-d1949ad7d7fd")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
